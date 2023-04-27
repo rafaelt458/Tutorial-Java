faea248b1dd961fec282f72f803840826df71308
unknown <rafaelt458@yahoo.es>
2023-04-27 18:26:59 +0200

    Parte 62 del tutorial

---
+++ Java062/src/main/java/com/laboratorio/java062/Java062.java
@@ -0,0 +1,12 @@
+package com.laboratorio.java062;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Java062 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
