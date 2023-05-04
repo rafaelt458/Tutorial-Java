768a60cce020d6d264313b21add9dd739cead3b2
unknown <rafaelt458@yahoo.es>
2023-05-04 18:19:38 +0200

    Parte 64 del tutorial

---
+++ Java064/src/main/java/com/laboratorio/java064/Java064.java
@@ -0,0 +1,43 @@
+package com.laboratorio.java064;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Java064 {
+    
+    public static void mostrarCiudades(Connection conn) {
+        Statement statement;
+        ResultSet rs;
+        int i = 0;
+        
+        try {
+            statement = conn.createStatement();
+            rs = statement.executeQuery("select * from city");
+            
+            while (rs.next()) {
+                i++;
+                System.out.println(i + "-) " + rs.getString("Name"));
+            }
+            
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        ConexionMySQL conexionMySQL = new ConexionMySQL();
+        Connection connection;
+        
+        connection = conexionMySQL.conectarMySQL();
+        if (connection != null) {
+            System.out.println("La conexión con MySql ha sido establecida!");
+            
+            mostrarCiudades(connection);
+            
+            conexionMySQL.cerrarConexionMySQL(connection);
+        } else {
+            System.out.println("Error al establer la conexión con MySql!");
+        }
+    }
+}
