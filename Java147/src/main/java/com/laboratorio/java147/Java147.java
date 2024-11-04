cfc2b36401338050657384b837e2f099ca668ea9
Rafael Tineo <rafaelt458@yahoo.es>
2024-11-04 18:34:25 +0100

    Partes 147, 148, 148, 150 y 151 del tutorial

---
+++ Java147/src/main/java/com/laboratorio/java147/Java147.java
@@ -0,0 +1,23 @@
+package com.laboratorio.java147;
+
+import com.laboratorio.java147.config.PersonaConfig;
+
+public class Java147 {
+
+    public static void main(String[] args) {
+        PersonaConfig config = PersonaConfig.getInstance();
+        System.out.println("Url: " + config.getProperty("url"));
+        
+        
+        String user = PersonaConfig.getInstance().getProperty("user");
+        System.out.println("User: " + user);
+        
+        
+        String clave = PersonaConfig.getInstance().getProperty("password");
+        System.out.println("Clave: " + clave);
+        
+        
+        String table = PersonaConfig.getInstance().getProperty("table");
+        System.out.println("Table: " + table);
+    }
+}
